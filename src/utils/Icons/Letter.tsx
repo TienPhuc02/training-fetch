c1aec213601325b4d21dfb4694f607978f44efc9
TienPhuc02 <dtp2352002@gmail.com>
2024-10-11 16:37:21 +0700

    fix bug letter icon

--- src/utils/Icons/Letter.tsx
+++ src/utils/Icons/Letter.tsx
@@ -9,7 +9,7 @@ const Letter = (props: { className: string }) => {
         viewBox="0 0 24 24"
         stroke-width="1.5"
         stroke="currentColor"
-        className="w-6 h-6 mr-[10px]"
+        {...props}
       >
         <path
           stroke-linecap="round"
