9bae2a08a1d86223c29d8097fa021b1006068ecd
TienPhuc02 <dtp2352002@gmail.com>
2024-10-14 11:40:56 +0700

    fix 1280

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -3,18 +3,19 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="header-container mx-auto w-full xl:max-w-[1200px] xl:max-h-[95px] xl:mb-[75px] lg:max-w-[900px] lg:h-[50px] lg:mb-[65px] sm:max-w-[300px] sm:h-[30px] sm:mb-[20px] ">
-      <div className="wrapper-header flex justify-between xl:items-center w-full h-full">
-        <div className="relative xl:w-[200px] xl:h-[95px] lg:w-[140px] sm:w-[80px] sm:h-auto">
+    <header className="header-container mx-auto w-full xl:max-w-[1200px] xl:h-[95px] xl:mb-[75px] lg:max-w-[900px] lg:h-[50px] lg:mb-[65px] sm:max-w-[300px] sm:h-[30px] sm:mb-[20px] ">
+      <div className="wrapper-header flex justify-between items-center xl:items-center w-full h-full">
+        <div className="xl:w-[200px] xl:h-[95px] lg:w-[140px] sm:w-[80px] sm:h-auto">
           <Image
             alt="logo"
             src="/images/logo-spinel.png"
             className="object-contain"
-            layout="fill"
+            width={580}
+            height={642}
             objectFit="contain"
           />
         </div>
-        <button className="font-primary bg-primary text-primary font-semibold xl:text-[20px] xl:py-[9px] xl:px-[16px] rounded-[5px] lg:text-[14px] lg:py-[9px] lg:px-[12px] sm:text-[12px] sm:py-[6px] sm:px-[10px] ">
+        <button className="font-primary bg-primary text-primary font-semibold xl:text-[20px] xl:py-[9px] xl:px-[16px] rounded-[5px] lg:text-[12px] lg:py-[7px] lg:px-[14px] sm:text-[12px] sm:py-[6px] sm:px-[10px] ">
           Launch App
         </button>
       </div>
